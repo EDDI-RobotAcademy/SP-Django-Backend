00cb1b65d6108deb14acf73d8b6ecd5bee1b2bdc
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-06-14 19:43:13 +0900

    feat: [이호준] 여행지 리스트를 출력합니다. [Django-TeamProject-2]

--- sp_project/first_project/travel/controller/views.py
+++ sp_project/first_project/travel/controller/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets, status
-
+from rest_framework.response import Response
 from travel.entity.models import Travel
+from travel.serializer import TravelSerializer
+from travel.service.travel_service_impl import TravelServiceImpl
 
 
 class TravelView(viewsets.ViewSet):
     queryset = Travel.objects.all()
+
+    travelService = TravelServiceImpl.getInstance()
+
+    def list(self, request):
+        travelList = self.travelService.list()
+        serializer = TravelSerializer(travelList, many=True)
+        return Response(serializer.data)
