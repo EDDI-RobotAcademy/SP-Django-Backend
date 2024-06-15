540f11a97a1dbae698a48adbead76ed0e2a205b0
jimin9703 <moses0322@naver.com>
2024-06-15 21:45:41 +0900

    feat: [김지민] 장장고 [sp]

--- sp_project/Jimin/first_project/travel/controller/views.py
+++ sp_project/Jimin/first_project/travel/controller/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-
 from travel.entity.models import Travel
 from travel.serializers import TravelSerializer
 from travel.service.travel_service_impl import TravelServiceImpl
@@ -16,3 +14,30 @@ class TravelView(viewsets.ViewSet):
         travelList = self.travelService.list()
         serializer = TravelSerializer(travelList, many=True)
         return Response(serializer.data)
+
+    def register(self, request):
+        try:
+            data = request.data
+
+            # productImage = request.FILES.get('productImage')
+            travelName = data.get('travelName')
+            travelLocation = data.get('travelLocation')
+            travelProperty = data.get('travelProperty')
+            travelContent = data.get('travelContent')
+            travelPrice = data.get('travelPrice')
+            travelImage = data.get('travelImage')
+
+
+            if not all([travelName, travelLocation, travelProperty, travelContent, travelPrice, travelImage]):
+                return Response({ 'error': '모든 내용을 채워주세요!' },
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            self.travelService.createTravel(travelName, travelLocation,
+                                              travelProperty, travelContent, travelPrice, travelImage)
+
+            serializer = TravelSerializer(data=request.data)
+            return Response(status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print('상품 등록 과정 중 문제 발생:', e)
+            return Response({ 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- sp_project/Jimin/first_project/travel/repository/travel_repository_impl.py
+++ sp_project/Jimin/first_project/travel/repository/travel_repository_impl.py
@@ -1,3 +1,6 @@
+import os
+
+from first_project import settings
 from travel.entity.models import Travel
 from travel.repository.travel_repository import TravelRepository
 
@@ -20,3 +23,24 @@ class TravelRepositoryImpl(TravelRepository):
 
     def list(self):
         return Travel.objects.all()
+
+    def create(self, travelName, travelLocation, travelProperty, travelContent, travelPrice, travelImage):
+        uploadDirectory = os.path.join(settings.BASE_DIR, '../../../../ui/Jimin/first/src/assets/images/uploadImages')
+        if not os.path.exists(uploadDirectory):
+            os.makedirs(uploadDirectory)
+
+        imagePath = os.path.join(uploadDirectory, travelImage.name)
+        with open(imagePath, 'wb+') as destination:
+            for chunk in travelImage.chunks():
+                destination.write(chunk)
+        travel = Travel(
+            travelName=travelName,
+            travelLocation=travelLocation,
+            travelProperty=travelProperty,
+            travelContent=travelContent,
+            travelPrice=travelPrice,
+            travelImage=travelImage.name
+        )
+        travel.save()
+        return travel
+
