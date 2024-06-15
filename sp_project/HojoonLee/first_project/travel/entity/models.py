24f8d71c12e8a04b01bc965795e133a71cac261d
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-06-15 15:40:17 +0900

    fix: [이호준] 각자 이름 넣어 코드 충돌 방지

--- sp_project/HojoonLee/first_project/travel/entity/models.py
+++ sp_project/HojoonLee/first_project/travel/entity/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Travel(models.Model):
     travelId = models.AutoField(primary_key=True) # AutoField == AutoIncrement 적용
     travelName = models.CharField(max_length=128, null=False) #
+    travelPrice = models.DecimalField(max_digits=10, decimal_places=2, default="0.0")
     travelLocation = models.CharField(max_length=32, null=False)
-    travelProperty = models.TextField() # 문자가 긴 경우 TextField 적용
+    travelProperty = models.TextField() #
     travelContent = models.TextField()
     travelImage = models.CharField(max_length=100, null=False)
 
--- sp_project/HojoonLee/first_project/travel/serializer.py
+++ sp_project/HojoonLee/first_project/travel/serializer.py
@@ -7,6 +7,6 @@ from travel.entity.models import Travel
 class TravelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Travel
-        fields = ['travelId', 'travelName',
+        fields = ['travelId', 'travelName', 'travelPrice',
                   'travelLocation', 'travelProperty', 'travelContent', 'travelImage']
 
